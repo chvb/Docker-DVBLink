33c2abcffefa753ec7f35c88114911ee5161b927
chvb <chvb@me.com>
2017-06-08 21:19:30 +0200

    Create dvblink.sh

--- etc/init.d/dvblink.sh
+++ etc/init.d/dvblink.sh
@@ -6,13 +6,13 @@ chmod -R 777 /opt-start/DVBLink
 chmod -R 777 /opt-start/dvblink
 
 # Prepare DVBLink bin
-if [ -z "`ls /usr/local/bin/dvblink --hide='lost+found'`" ]
+if [ -z "`ls /usr/local/bin/dvblink/config --hide='lost+found'`" ]
 then
-	cp -R /opt-start/dvblink/* /usr/local/bin/dvblink
+	cp -R /opt-start/dvblink/* /usr/local/bin/dvblink/config
 fi
-if [ ! -f "/usr/local/bin/dvblink/dvblink_server" ]
+if [ ! -f "/usr/local/bin/dvblink/config/dvblink_settings.xml" ]
 then
-	cp -Rn /opt-start/dvblink/* /usr/local/bin/dvblink/
+	cp -Rn /opt-start/dvblink/* /usr/local/bin/dvblink/config/
 fi
 
 # Prepare DVBLink bin
